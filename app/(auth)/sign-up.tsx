1213c817c69933127c2c9bd016ba7c17e3cd06f6
ivanbutakov0 <ivanbutakov000@gmail.com>
2024-11-05 19:49:24 +0500

    connect to db and and implement user creation api

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,6 +2,7 @@ import CustomButton from "@/components/CustomButton"
 import InputField from "@/components/InputField"
 import OAuth from "@/components/OAuth"
 import { icons, images } from "@/constants"
+import { fetchAPI } from "@/lib/fetch"
 import { useSignUp } from "@clerk/clerk-expo"
 import { Link, router } from "expo-router"
 import { useState } from "react"
@@ -64,7 +65,18 @@ const SignUp = () => {
       })
 
       if (completeSignUp.status === "complete") {
-        // TODO: Create a database user!
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          }),
+        })
+
         await setActive({ session: completeSignUp.createdSessionId })
         setVerification({
           ...verification,
@@ -144,8 +156,6 @@ const SignUp = () => {
             <Text>Already have an account?</Text>
             <Text className="text-primary-500 font-bold"> Log in</Text>
           </Link>
-
-          {/* TODO: Create another modal for test verification */}
 
           <ReactNativeModal
             isVisible={verification.state === "pending"}
